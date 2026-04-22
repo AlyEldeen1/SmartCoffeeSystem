b7a36d7af5d77f119666066d2a0cd1cee30d09b5
Aly Eldeen <alieldeen2012@gmail.com>
2026-04-22 22:43:25 +0200

    intial backend setup

---
+++ src/app.js
@@ -0,0 +1,10 @@
+// backend brain
+const express = require('express'); // import express tool
+const app = express(); // create an express app
+app.use(express.json()); // enable json
+
+app.get("/health", (req, res) => {
+    res.json({ status: "ok", message: "Backend Running" });
+});
+
+module.exports = app; // export for server.js to use
